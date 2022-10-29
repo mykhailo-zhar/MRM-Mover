51fec11470cfa83f37d208aa2aebe0641f98839a
MrZahn <mykhailo.zhar@stud.onu.edu.ua>
2022-10-29 11:02:18 +0300

    Inital

---
+++ MPM_Lab_2/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace MPM_Lab_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Command = new System.Windows.Forms.TextBox();
+            this.IdentifyButton = new System.Windows.Forms.Button();
+            this.ProcessButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.ClrButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Command
+            // 
+            this.Command.Location = new System.Drawing.Point(31, 27);
+            this.Command.Multiline = true;
+            this.Command.Name = "Command";
+            this.Command.Size = new System.Drawing.Size(152, 84);
+            this.Command.TabIndex = 0;
+            // 
+            // IdentifyButton
+            // 
+            this.IdentifyButton.Location = new System.Drawing.Point(31, 146);
+            this.IdentifyButton.Name = "IdentifyButton";
+            this.IdentifyButton.Size = new System.Drawing.Size(70, 23);
+            this.IdentifyButton.TabIndex = 1;
+            this.IdentifyButton.Text = "Identify";
+            this.IdentifyButton.UseVisualStyleBackColor = true;
+            // 
+            // ProcessButton
+            // 
+            this.ProcessButton.Location = new System.Drawing.Point(31, 176);
+            this.ProcessButton.Name = "ProcessButton";
+            this.ProcessButton.Size = new System.Drawing.Size(152, 23);
+            this.ProcessButton.TabIndex = 2;
+            this.ProcessButton.Text = "Process";
+            this.ProcessButton.UseVisualStyleBackColor = true;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(31, 205);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(70, 23);
+            this.SaveButton.TabIndex = 3;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            // 
+            // ClrButton
+            // 
+            this.ClrButton.Location = new System.Drawing.Point(112, 146);
+            this.ClrButton.Name = "ClrButton";
+            this.ClrButton.Size = new System.Drawing.Size(71, 23);
+            this.ClrButton.TabIndex = 4;
+            this.ClrButton.Text = "Clear";
+            this.ClrButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 5;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.LoadButton);
+            this.groupBox1.Controls.Add(this.ProcessButton);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.Command);
+            this.groupBox1.Controls.Add(this.ClrButton);
+            this.groupBox1.Controls.Add(this.IdentifyButton);
+            this.groupBox1.Controls.Add(this.SaveButton);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.groupBox1.Location = new System.Drawing.Point(600, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 450);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(113, 205);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(70, 23);
+            this.LoadButton.TabIndex = 6;
+            this.LoadButton.Text = "Save";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(31, 117);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 23);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(144, 399);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "MPM Console";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Command;
+        private System.Windows.Forms.Button IdentifyButton;
+        private System.Windows.Forms.Button ProcessButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button ClrButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
